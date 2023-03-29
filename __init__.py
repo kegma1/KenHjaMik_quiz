3df77f1cbedf4c40e6c5020537e7a1b6cc2ece6e
kegma1 <61615726+kegma1@users.noreply.github.com>
2023-03-29 15:14:03 +0200

    removed methods from index

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.secret_key = secrets.token_urlsafe(16)
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
 def index():
     return render_template("index.html", title="login")
 
